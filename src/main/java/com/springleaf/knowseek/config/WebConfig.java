b18d1b4a57ea002a4f59986a2208ecbf500294ca
springleaf <3313252209@qq.com>
2025-08-19 23:29:44 +0800

    style: 修改WebConfig包名从common到config

--- src/main/java/com/springleaf/knowseek/config/WebConfig.java
+++ src/main/java/com/springleaf/knowseek/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.springleaf.knowseek.common;
+package com.springleaf.knowseek.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
